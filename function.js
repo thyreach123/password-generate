16b093102cd0265e68a8a18a098a79ec2781ad14
sokunthyreach yun <yunsokunthyreach@gmail.com>
2024-08-14 12:05:41 +0700

    passgen

---
+++ function.js
@@ -0,0 +1,35 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+
+const upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()><?{}[]+=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+        password += upperCase[Math.floor(Math.random() * upperCase.length)];
+        password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        password += number[Math.floor(Math.random() * number.length)];
+        password += symbol[Math.floor(Math.random() * symbol.length)];
+
+        while(lenght > password.length){
+            password += allChars[Math.floor(Math.random() * allChars.length)];
+        }
+
+        passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    navigator.clipboard.writeText(passwordBox.value);
+
+    let copyStatus = document.getElementById("copyStatus");
+        copyStatus.style.display = "block";
+        setTimeout(function() {
+            copyStatus.style.display = "none";
+        }, 2000);
+
+}
